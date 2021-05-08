32e4fa9c751cfacbe8ea172cc0539de7ac7b0b01
Boliev <voff.web@gmail.com>
2021-05-08 13:08:45 +0300

    add config files

--- coach.go
+++ coach.go
@@ -3,6 +3,7 @@ package coach
 import (
 	"github.com/boliev/coach/internal/controller"
 	"github.com/boliev/coach/internal/mongo"
+	"github.com/boliev/coach/pkg/config"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,8 +11,13 @@ type App struct {
 }
 
 func (app App) Start() {
-	mongoClient := mongo.NewClient("mongodb://coach:123456@localhost:27017")
-	userRepository := mongo.NewUserMongoRepository(mongoClient, "coach", "user")
+	config := getConfig()
+	mongoClient := mongo.NewClient(config.GetString("mongo_uri"))
+	userRepository := mongo.NewUserMongoRepository(
+		mongoClient,
+		config.GetString("main_database"),
+		config.GetString("users_collection"),
+	)
 
 	r := gin.New()
 	v1 := r.Group("/v1")
@@ -29,4 +35,13 @@ func (app App) Start() {
 		}
 	}
 	r.Run()
+}
+
+func getConfig() *config.Config {
+	config, err := config.NewConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return config
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+}
+
+func NewConfig() (*Config, error) {
+	viper.SetConfigName("config.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {
+		return nil, fmt.Errorf("fatal error config file: %s ", err.Error())
+	}
+
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("./")
+	err = viper.MergeInConfig() // Find and read the config file
+	if err != nil {
+		return nil, fmt.Errorf("fatal error config file: %s ", err.Error())
+	}
+
+	if _, err := os.Stat("./.env.local"); err == nil {
+		viper.SetConfigName(".env.local")
+		err = viper.MergeInConfig() // Find and read the config file
+		if err != nil {
+			return nil, fmt.Errorf("fatal error config file: %s ", err.Error())
+		}
+	}
+	return &Config{}, nil
+}
+
+func (c Config) GetString(key string) string {
+	return viper.GetString(key)
+}
